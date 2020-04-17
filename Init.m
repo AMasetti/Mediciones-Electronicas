ab81e69d78a07e3fc83880486f910a4ee0843138
AMasetti <32473909+AMasetti@users.noreply.github.com>
2020-04-17 20:58:58 -0300

    Initial commit

---
+++ Init.m
@@ -0,0 +1,6 @@
+%% Contador Universal
+Fr = 1000000; %HZ
+Fincognita = 25000; %Hz
+
+%% Variables necesarias para CVF
+Tm = 0.1/5;
---
+++ README.md
@@ -0,0 +1,2 @@
+# Mediciones-Electronicas
+ Circuitos Basicos de los apuntes de la Catedra de Mediciones Electronics de la FCEIA
